fd1fb89e5bdd722bc201dec23649627c0c864704
PAVELchicharacha <pavelkiprov@yandex.ru>
2023-10-04 00:14:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿class program
+{
+    //метод вычисления наибольшего общего делителя
+    public static int NOD(int a, int b)
+    {
+        while (a != b)
+        {
+            if (a > b)
+                a -= b;
+            else
+                b -= a;
+        }
+        return a;
+    }
+    public static void Main()
+    {
+        //ввод числителя, знаминателя дроби, и их копий
+        Console.WriteLine("Введите числитель дроби:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите знаминатель дроби:");
+        int b = int.Parse(Console.ReadLine());
+        //вывод сокращенной дроби
+        Console.WriteLine(" ");
+        Console.WriteLine(a / NOD(a, b));
+        Console.WriteLine(b / NOD(a, b));
+    }
+}
